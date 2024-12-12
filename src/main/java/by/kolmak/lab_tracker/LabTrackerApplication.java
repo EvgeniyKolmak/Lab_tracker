685f1653adb398a35296e4028f9ac5610db30642
Evgeniy Kolmak <zenakolmak669@gmail.com>
2024-12-12 13:59:27 +0300

    first commit

---
+++ src/main/java/by/kolmak/lab_tracker/LabTrackerApplication.java
@@ -0,0 +1,13 @@
+package by.kolmak.lab_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kolmak/lab_tracker/LabTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kolmak.lab_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
